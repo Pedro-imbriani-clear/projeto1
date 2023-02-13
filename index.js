ecb127be6fdefd7f8103538dc0aa505def1a1e40
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-02-13 14:22:24 -0300

    declaraçao e comparaçao das variaveis

---
+++ index.js
@@ -0,0 +1,19 @@
+var olamundo = "hello word"
+let a = 10;
+let cor = "amarelo";
+const b = '10';
+console.log(olamundo);
+console.log(olamundo);
+console.log(olamundo);
+console.log(olamundo);
+console.log(a == b ||  typeof a == 'string') ;
+
+if(cor === "verde"){
+    console.log("siga");
+}
+    else if(cor === "amarelo"){
+        console.log("atençao");
+    }
+else{
+    console.log("pare");
+}
